41ac6efd7b7a81d713edb3d0b462ab1646a7db1f
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2025-01-03 21:51:07 -0800

    production ready now

--- index.ts
+++ index.ts
@@ -2,29 +2,26 @@ import express, { Response } from "express"
 import OpenAI from "openai"
 import languages from "./list.json"
 import { languages as language_type } from "./types"
-import { _decodeChunks } from "openai/streaming.mjs"
 
 const app = express()
 let ai = new OpenAI({ apiKey: process.env.OPENAI_KEY })
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 
 function createWritableStream(response: Response) {
     return new WritableStream({
-      write(chunk: Uint8Array) {
-        let processedChunk: OpenAI.Chat.Completions.ChatCompletionChunk = JSON.parse(Buffer.from(chunk).toString())
-        response.write(processedChunk.choices[0].delta.content || "");
-      },
-      close() {
-        response.end();
-      },
-      abort(err) {
-        console.error('Stream aborted:', err);
-        response.status(500).json({error: "500 INTERNAL SERVER ERROR", message: "Our AI Model may be down as of right now, please be patient."})
-      }
+        write(chunk: Uint8Array) {
+            let processedChunk: OpenAI.Chat.Completions.ChatCompletionChunk = JSON.parse(Buffer.from(chunk).toString())
+            response.write(processedChunk.choices[0].delta.content || "");
+        },
+        close() {
+            response.end();
+        },
+        abort() {
+            response.status(500).json({ error: "500 INTERNAL SERVER ERROR", message: "Our AI Model is having some issues, please be patient." })
+        }
     });
-  }
+}
 
 async function getCompletion(prompt: string) {
     try {
@@ -61,15 +58,15 @@ app.post("/query-language", async (req, res) => {
     let language: string = req.body.language
     let difficulty: number = parseInt(req.body.difficulty ?? "")
     let questions: number = parseInt(req.body.questions ?? "")
-    if(!language || !difficulty || !questions) {
-        res.status(400).json({ error: "400 BAD REQUEST", message: "Make sure you input an appropriate language and difficulty number" })
+    if (!language || !difficulty || !questions) {
+        res.status(400).json({ error: "400 BAD REQUEST", message: "Make sure you input an appropriate language, questions, and difficulty number" })
         return;
     }
-    if(difficulty > 10 || difficulty < 0) {
+    if (difficulty > 10 || difficulty < 0) {
         res.status(400).json({ error: "400 BAD REQUEST", message: "Difficulty number out of range" })
         return;
     }
-    if(questions < 1 || questions > 50) {
+    if (questions < 1 || questions > 50) {
         res.status(400).json({ error: "400 BAD REQUEST", message: "Questions number out of range" })
         return;
     }
@@ -81,12 +78,11 @@ app.post("/query-language", async (req, res) => {
     try {
         res.setHeader("Content-Type", "application/json")
         let comp = await getCompletion(`Provide exactly ${questions} MCQ, no more and no less, with 4 possible answers with difficulty of ${difficulty}/10 for the programming language ${language}. Provide only JSON with questions in the "questions" array, store the question number in the "number" field, store the answer as an index in the field "answer", store possible answers in the "options" field, store the question in the "question" field, and escape when needed. Please avoid formatting the code block and just send the raw code for these ${questions} questions.`);
-        if(!comp) throw new Error()
+        if (!comp) throw new Error()
         let stream = createWritableStream(res)
         comp.pipeTo(stream)
-    } catch(_) {
-        console.log(_)
-        res.status(500).json({error: "500 INTERNAL SERVER ERROR", message: "Our AI Model may be down as of right now, please be patient."})
+    } catch (_) {
+        res.status(500).json({ error: "500 INTERNAL SERVER ERROR", message: "Our AI Model is having some issues, please be patient." })
         return;
     }
 })
